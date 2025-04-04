c4a81b6e449b01c7acfc9b74c3f13b8a503f303b
NIKHIL SINGH <nikhil1108singh@gmail.com>
2025-04-04 21:11:51 +0530

    Primitive_data_types

---
+++ Primitive_data_types.java
@@ -0,0 +1,12 @@
+public class Primitive_data_types{
+    public static void main(String[]args){
+        int roll_no = 58;
+        char letter = 'g';
+        float marks = 85.8f;
+        double large_decimal_number = 539859358935.35;
+        long large_integer = 53533563534635L;
+        boolean check = false;
+        System.out.println(roll_no);
+    }
+// Primitive data type are those that cannot be divided any furthur
+}
