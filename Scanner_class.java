5db5e50e025e72d8e25bfd17e82845cea5d08e5a
NIKHIL SINGH <nikhil1108singh@gmail.com>
2025-04-04 21:15:55 +0530

    Scanner_class

---
+++ Scanner_class.java
@@ -0,0 +1,11 @@
+//package doc.java+
+
+import java.util.Scanner;
+
+public class Scanner_class {
+    public static void main (String[]args){
+        Scanner input = new Scanner(System.in);
+        
+        System.out.println(input.nextLine());
+    }
+}
