6c2ea84b431898d31b7b7528fedd6f0806b45d86
NIKHIL SINGH <nikhil1108singh@gmail.com>
2025-04-04 18:17:27 +0530

    Main_with_comments

--- Main.java
+++ Main.java
@@ -1,4 +1,7 @@
+//The file name should be same as the class name
 public class Main {
+    // As the java is case sensitive thus some letters should starts with capital letters
+    //such as String , System , etc.
     public static void main (String[]args){
         System.out.println("Hello World");
     }
